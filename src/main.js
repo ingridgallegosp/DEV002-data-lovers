63638f2225cb48e56b80213e834b6e588b7dfb74
Melanie Tolvett S <melanietolvett@gmail.com>
2022-11-16 09:18:18 -0300

    obteniendo Id al clickear

--- src/main.js
+++ src/main.js
@@ -6,8 +6,16 @@
 //console.log(example, data);
 
 const librosBtn = document.getElementById('librosBtn');
-    const contenidoLibros = document.getElementById('contenidoLibros');
+const contenidoLibros = document.getElementById('contenidoLibros');
 
-    librosBtn.addEventListener('click', function () {
-      contenidoLibros.classList.add('mostrar-contenido');
-    })
+librosBtn.addEventListener('click', function () {
+  contenidoLibros.style.display = "block";
+})
+
+document.querySelectorAll(".click").forEach(el => {
+  el.addEventListener("click", e => {
+    const id = e.target.getAttribute("id");
+    console.log("Se ha clickeado el id " + id);
+  });
+});
+//const idContenido =
