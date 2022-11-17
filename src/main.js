5050977a2bc851730a363814295979d0300bd6c4
Melanie Tolvett S <melanietolvett@gmail.com>
2022-11-17 17:18:28 -0300

    maqueta HU bienvenida y personajes

--- src/main.js
+++ src/main.js
@@ -5,17 +5,9 @@
 
 //console.log(example, data);
 
-const librosBtn = document.getElementById('librosBtn');
-const contenidoLibros = document.getElementById('contenidoLibros');
+const homeBtn = document.getElementById('homeBtn');
+const contenidoHome = document.getElementById('contenidoHome');
 
-librosBtn.addEventListener('click', function () {
-  contenidoLibros.style.display = "block";
-})
-
-document.querySelectorAll(".click").forEach(el => {
-  el.addEventListener("click", e => {
-    const id = e.target.getAttribute("id");
-    console.log("Se ha clickeado el id " + id);
-  });
-});
-//const idContenido =
+homeBtn.addEventListener('click', function () {
+ contenidoHome.style.display = "flex";
+ })
