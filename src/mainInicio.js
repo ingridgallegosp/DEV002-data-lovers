2f4d425ca7e84f172f97933850c93dea486c643c
Ingrid G <ingridgape18@hotmail.com>
2022-11-17 15:33:38 -0500

    se agrega carpeta imagenes y  nuevo css js y html de inicio

---
+++ src/mainInicio.js
@@ -0,0 +1,19 @@
+//import { example } from './data.js';
+// import data from './data/lol/lol.js';
+//import data from './data/pokemon/pokemon.js';
+// import data from './data/rickandmorty/rickandmorty.js';
+
+//console.log(example, data);
+
+// IR A PAGINA DE BIENVENIDA
+document.getElementById('empezar').onclick = 
+  function (){
+      let nombrex = document.getElementById('nombreInvitado').value;
+    console.log (nombrex)
+
+      if(!nombrex) {
+      alert( "No olvides ingresar tu nombre") ;
+        } else {
+         window.location.href='indexInicio.html' ; 
+        } ;
+};
