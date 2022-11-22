418489ae0c379733c158f4136d85bc063f0b9296
Ingrid G <ingridgape18@hotmail.com>
2022-11-22 18:53:47 -0500

    pruebas con tarjetas

--- src/mainContenido.js
+++ src/mainContenido.js
@@ -1,17 +1,17 @@
 //import { example } from './data.js';
-// import data from './data/lol/lol.js';
 //import data from './data/pokemon/pokemon.js';
+//import { filtrarDirector, filtrarProductor, ordenPeliculas, calculo } from './data.js';
+
+
 import data from './data/harrypotter/harry.js';
-
-
-//console.log(example, data);
-
 
 const homeBtn = document.getElementById('homeBtn'); //capturando homeBtn
 const contenidoHome = document.getElementById('contenidoHome'); //capturando contenidoHome
-
+const contenidoPersonajes = document.getElementById('contenidoPersonajes')//capturando contenidoPersonajes
 const personajesBtn = document.getElementById('personajesBtn'); //capturando personajesBtn
-const contenidoPersonajes = document.getElementById('contenidoPersonajes'); //capturando contenidoPersonajes
+
+
+
 
 //Evento click de homeBtn despliega contendioHome y oculta contenidoPersonajes
 homeBtn.addEventListener('click', function () {
@@ -23,7 +23,38 @@ homeBtn.addEventListener('click', function () {
 personajesBtn.addEventListener('click', function () {
   contenidoPersonajes.style.display = "flex";
   contenidoHome.style.display = "none";
+
+
+
+
+//pintar tarjetas
+crearTarjetas (data.characters);
+function crearTarjetas (element) {
+  console.log(element)
+  
+  const container = document.querySelector(".tarjeta");
+  element.forEach(characters => {
+    container.innerHTML += `
+            <article class="card" id= "card">
+              <h2> ${characters.name}</h2>
+              <p> ${characters.house}</p>
+            </article>
+            `
+  });
+  
+};
+console.log(data.characters);
+
+//cierra pintar tarjetas*/
 })
+
+
+
+
+
+
+
+
 
 
 // IR A PAGINA DE INICIO
