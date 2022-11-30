8cb409bb80a9bae4be243e03451aac050dfd30b0
Melanie Tolvett S <melanietolvett@gmail.com>
2022-11-29 21:34:28 -0300

    funcion ordenar check

--- src/mainContenido.js
+++ src/mainContenido.js
@@ -10,25 +10,22 @@ const contenidoHome = document.getElementById('contenidoHome'); //capturando con
 const homeBtn = document.getElementById('homeBtn'); //capturando homeBtn
 const personajesBtn = document.getElementById('personajesBtn'); //capturando personajesBtn
 const salidaBtn = document.getElementById('salidaBtn'); //capturando salidaBtn
-var personajesHP = ""
-var prueba = 0
-
-salidaBtn.addEventListener('click', function () {
-  window.location.href = "index.html";
-})
 
 //Guardar valor - nombre del invitado
 //localStorage.setItem("nombreM");
 //localStorage.removeItem("nombreM")
 
-//Obtener valor almacenado local Storage 
-let nombreObtenido = localStorage.getItem("nombreM");
+
+let nombreObtenido = localStorage.getItem("nombreM");//Obtener valor almacenado local Storage 
 //console.log(nombreObtenido)
 
-//Mostrar valor almacenado  
-let nombreBienv = document.getElementById("mensaje-bienvenida");
+
+let nombreBienv = document.getElementById("mensaje-bienvenida");//Mostrar valor almacenado  
 nombreBienv.innerHTML = "Bienvenida " + nombreObtenido;
 
+salidaBtn.addEventListener('click', function () {
+  window.location.href = "index.html";
+})
 
 // MANIPULACIÓN PÁGINA INDEX/CONTENIDO
 
@@ -48,13 +45,14 @@ personajesBtn.addEventListener('click', function () {
 //export funcion que define personajes :( no funciona
 let charactersHP = [];
 charactersHP = data.characters;
-console.log(charactersHP);
-console.log(prueba);
+//console.log(charactersHP);
+
 
 // pintar tarjetas
-let tarjetas = []; //vamos a ir inyectando los valores de abajo
+ //vamos a ir inyectando los valores de abajo
 function crearTarjetas(element) {
   //console.log(element);
+  let tarjetas = [];
   element.forEach(characters => { //template literaios
     tarjetas += ` 
               <div class="tarjetas" id= ${characters.id}>
@@ -73,7 +71,7 @@ function crearTarjetas(element) {
   });
   return tarjetas
 };
-console.log(personajesHP)
+//console.log(personajesHP)
 
 // PRUEBA PARA DIVIDIR CODIGO
 
@@ -98,20 +96,50 @@ const ordenarAsc = (array) => {
 };
 //console.log(ordenarAsc(charactersHP));
 
-function ordenarDesc(array) {
+const ordenarDesc = (array) => {
   const descendente = array.sort(function (a, b) {
     return b.name.localeCompare(a.name);
   });
   return descendente;
 }
 
-const ordenAscendente = document.getElementById('asc'); //capturando opcion ascendente
+const select = document.querySelector("#orden") //capturando opcion ascendente
 //escoger opcion a-z y z-a 
-ordenAscendente.addEventListener('change', function () {
-  console.log(this.options[select.selectedIndex]
-  )
+console.log(select);
+
+const opcionCambiada = () => {
+  console.log("cambio");
   //contenidoPersonajes.innerHTML = crearTarjetas(ordenarAsc(data.characters.name));
+};
+select.addEventListener("change", opcionCambiada);
+
+const mostrar = () => {
+  const indice = select.selectedIndex;
+  if (indice === -1) return; // Esto es cuando no hay elementos
+  const opcionSeleccionada = select.options[indice];
+  alert(`Texto: ${opcionSeleccionada.text}. Valor: ${opcionSeleccionada.value}`);
+};
+/*
+document.querySelector("#ir").addEventListener('click', function () {
+  contenidoPersonajes.innerHTML = crearTarjetas(ordenarAsc(charactersHP))
+});*/
+
+document.getElementById("ir").addEventListener('click', function () {
+  let indice = select.selectedIndex;
+  console.log(indice)
+  if (indice == 1) {
+    contenidoPersonajes.innerHTML = ""
+    contenidoPersonajes.innerHTML = crearTarjetas(ordenarAsc(charactersHP));
+    console.log(crearTarjetas(ordenarAsc(charactersHP)))
+  }
+  else if (indice == 2) {
+    contenidoPersonajes.innerHTML = ""
+    contenidoPersonajes.innerHTML = crearTarjetas(ordenarDesc(charactersHP))
+    console.log(crearTarjetas(ordenarDesc(charactersHP)))
+  }
 });
+
+
 
 
 //console.log(ordenarAsc(charactersHP));
