321ba7f3d981b14706f6a004878f2a89f5e1acd6
Melanie Tolvett S <melanietolvett@gmail.com>
2022-12-01 09:35:47 -0300

    arreglos a la interfaz, orden codigo y evento load de window agregado

--- src/mainContenido.js
+++ src/mainContenido.js
@@ -1,34 +1,35 @@
 //import { filterAsc, filterDesct } from './data.js'; //organizarPersonaje
 import data from './data/harrypotter/harry.js';
-import { ordenarAsc, ordenarDesc} from './data.js';
+import { ordenarAsc, ordenarDesc } from './data.js';
 
 //console.log(example, data);
 
-const contenidoPersonajes = document.getElementById('contenidoPersonajes'); //capturando contenidoPersonajes
 const contenidoHome = document.getElementById('contenidoHome'); //capturando contenidoHome
+const contenidoPersonajes = document.getElementById('contenidoPersonajes'); //capturando contenidoPersonajes
+const contenidoTarjetas = document.getElementById("tarjetas");
 const homeBtn = document.getElementById('homeBtn'); //capturando homeBtn
 const personajesBtn = document.getElementById('personajesBtn'); //capturando personajesBtn
 const salidaBtn = document.getElementById('salidaBtn'); //capturando salidaBtn
 
-
-let nombreObtenido = localStorage.getItem("nombreM");//Obtener valor almacenado local Storage 
-//console.log(nombreObtenido)
-let nombreBienv = document.getElementById("mensaje-bienvenida");//Mostrar valor almacenado  
-nombreBienv.innerHTML = "Bienvenida " + nombreObtenido;
-
+window.addEventListener("load", function () {
+  let nombreObtenido = localStorage.getItem("nombreM");//Obtener valor almacenado local Storage 
+  //console.log(nombreObtenido)
+  let nombreBienv = document.getElementById("mensaje-bienvenida");//Mostrar valor almacenado  
+  nombreBienv.innerHTML = "Bienvenida " + nombreObtenido;
+});
 
 salidaBtn.addEventListener('click', function () {
   window.location.href = "index.html";
-})
+});
 homeBtn.addEventListener('click', function () {
   contenidoHome.style.display = "flex";
   contenidoPersonajes.style.display = "none";
-})
+});
 personajesBtn.addEventListener('click', function () {
-  contenidoPersonajes.style.display = "flex";
+  contenidoPersonajes.style.display = "block";
   contenidoHome.style.display = "none";
-  contenidoPersonajes.innerHTML = crearTarjetas(data.characters);
-})
+  contenidoTarjetas.innerHTML = crearTarjetas(ordenarAsc(charactersHP));
+});
 
 let charactersHP = [];
 charactersHP = data.characters;
@@ -41,8 +42,8 @@ function crearTarjetas(element) {
   let tarjetas = [];
   element.forEach(characters => { //template literaios
     tarjetas += ` 
-              <div class="tarjetas" id= ${characters.id}>
-                <div class= "texto-tarjetas">
+              <div class="tarjeta" id= ${characters.id}>
+                <div class= "texto-tarjeta">
                 <h2 class="nombre">${characters.name}</h2>
                 <li class="casa"> Casa: ${characters.house}</li>
                 <li class="especie">Especie: ${characters.species}</li>
@@ -58,37 +59,25 @@ function crearTarjetas(element) {
   return tarjetas
 };
 
+//Seleccion para ordenar alfabeticamente
 const select = document.querySelector("#orden") //capturando opcion ascendente
 //escoger opcion a-z y z-a 
 console.log(select);
-
-document.getElementById("ir").addEventListener('click', function () {
+//Evento change llama a la funcion segun indice de seleccion
+document.getElementById("orden").addEventListener('change', function () {
   let indice = select.selectedIndex;
-  console.log(indice)
+  //console.log(indice)
   if (indice == 1) {
-    contenidoPersonajes.innerHTML = ""
-    contenidoPersonajes.innerHTML = crearTarjetas(ordenarAsc(charactersHP));
-    console.log(crearTarjetas(ordenarAsc(charactersHP)))
+    contenidoTarjetas.innerHTML = ""
+    contenidoTarjetas.innerHTML = crearTarjetas(ordenarAsc(charactersHP));
+    //console.log(crearTarjetas(ordenarAsc(charactersHP)))
   }
   else if (indice == 2) {
-    contenidoPersonajes.innerHTML = ""
-    contenidoPersonajes.innerHTML = crearTarjetas(ordenarDesc(charactersHP))
-    console.log(crearTarjetas(ordenarDesc(charactersHP)))
+    contenidoTarjetas.innerHTML = ""
+    contenidoTarjetas.innerHTML = crearTarjetas(ordenarDesc(charactersHP))
+    //console.log(crearTarjetas(ordenarDesc(charactersHP)))
   }
 });
 
 
-const opcionCambiada = () => {
-  console.log("cambio");
-  //contenidoPersonajes.innerHTML = crearTarjetas(ordenarAsc(data.characters.name));
-};
-
-select.addEventListener("change", opcionCambiada);
-
-const mostrar = () => {
-  const indice = select.selectedIndex;
-  if (indice === -1) return; // Esto es cuando no hay elementos
-  const opcionSeleccionada = select.options[indice];
-  alert(`Texto: ${opcionSeleccionada.text}. Valor: ${opcionSeleccionada.value}`);
-};
 
